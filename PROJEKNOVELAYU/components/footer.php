615a80e4b111709e58cc6b802d8696ac587b3595
Ayu Ainun A'ziziyyah <116784668+ayuainun@users.noreply.github.com>
2023-03-13 19:22:12 +0700

    Add files via upload

---
+++ PROJEKNOVELAYU/components/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:ayuainun0@gmail.com">ayuainun0@gmail.com</a>
+		 <a href="mailto:ayuzizi@gmail.com">ayuzizi@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 09:00pm</p>
+		 <p>Weekend Close</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <p>Kertosono, Nganjuk <br> Jawa Timur</p>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+        <a href="tel:1234567899">123-456-7899</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+  <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Ayu Ainun A'ziziyyah</span> | all rights reserved!</div>
+
+</footer>
---
+++ PROJEKNOVELAYU/update_address.php
@@ -0,0 +1,82 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+   header('location:home.php');
+};
+
+if(isset($_POST['submit'])){
+
+   $address = $_POST['house'] .', '.$_POST['street'].', '. $_POST['city'] .', '. $_POST['country'] .' - '. $_POST['pin_code'];
+   $address = filter_var($address, FILTER_SANITIZE_STRING);
+
+   $update_address = $conn->prepare("UPDATE `users` set address = ? WHERE id = ?");
+   $update_address->execute([$address, $user_id]);
+
+   $message[] = 'address saved!';
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>update address</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php' ?>
+
+<section class="form-container">
+
+   <form action="" method="post">
+      <h3>your address</h3>
+      <input type="text" class="box" placeholder="house no. e.g. 10" required maxlength="50" name="house">
+      <input type="text" class="box" placeholder="street name e.g. Jl Melati" required maxlength="50" name="street">
+      <input type="text" class="box" placeholder="city name e.g. Nganjuk" required maxlength="50" name="city">
+      <input type="text" class="box" placeholder="country name e.g. Indonesia" required maxlength="50" name="country">
+      <input type="number" class="box" placeholder="pin code" required max="999999" min="0" maxlength="6" name="pin_code">
+      <input type="submit" value="save address" name="submit" class="btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php' ?>
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
